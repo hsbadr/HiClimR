c54676453ae3306c5b89ca46da51f8e54de73ad1
Hamada S. Badr <hamada.s.badr@gmail.com>
2018-12-22 15:57:37 -0500

    Fix NOTE: Registering native routines
    File ‘HiClimR/libs/HiClimR.so’:
      Found no calls to: ‘R_registerRoutines’, ‘R_useDynamicSymbols’
    
    It is good practice to register native routines and to disable symbol search.
    
    Use the output from:
        tools::package_native_routine_registration_skeleton('./HiClimR')

---
+++ src/HiClimR_init.c
@@ -0,0 +1,24 @@
+#include <R.h>
+#include <Rinternals.h>
+
+#include <R_ext/RS.h>
+#include <R_ext/Rdynload.h>
+
+#include <stdlib.h> // for NULL
+
+/* .Fortran calls */
+void F77_SUB(hiclimr)(int *n, int *len, int *iopt, int *ia, int *ib,
+		double *crit, double *membr, double *var, double *diss);
+void F77_SUB(hcass2)(int *n, int *ia, int *ib, int *iorder, int *iia, int *iib);
+
+static const R_FortranMethodDef FortranEntries[] = {
+    {"hiclimr", (DL_FUNC) &F77_NAME(hiclimr), 9},
+    {"hcass2",  (DL_FUNC) &F77_NAME(hcass2),  6},
+    {NULL, NULL, 0}
+};
+
+void R_init_HiClimR(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, NULL, FortranEntries, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
